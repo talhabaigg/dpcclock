00819cf07aad16e45e29df278bb8209357f6ad3e
Talha Baig <iamtalhabaig@gmail.com>
2025-05-14 15:09:07 +1000

    Enable sync process to EH hourly

--- routes/console.php
+++ routes/console.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Artisan;
 use App\Console\Commands\SyncTimesheets;
 
 
-// Schedule::command(SyncTimesheets::class)
-//     ->everyMinute()
-//     ->timezone('Australia/Brisbane');
+Schedule::command(SyncTimesheets::class)
+    ->hourly()
+    ->timezone('Australia/Brisbane');
 
 Schedule::command('app:send-kiosk-clocked-in-notification')->hourly()
     ->timezone('Australia/Brisbane')
